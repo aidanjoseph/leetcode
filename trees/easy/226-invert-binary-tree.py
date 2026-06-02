5c8e9715d5b84a088af1dc38dba28aed5f8215b0
Aidan Joseph <aj626@cornell.edu>
2026-06-02 19:32:54 -0400

    think in terms of base cases, non tbad

---
+++ trees/easy/226-invert-binary-tree.py
@@ -0,0 +1,20 @@
+'''
+not really that bad
+just think in terms of base cases
+precurosor to 101 kinda
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+        
