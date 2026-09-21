d31785df099a2f2645fb17d7f9cfc20bc5744d41
Aidan Joseph <aj626@cornell.edu>
2026-09-21 16:45:40 -0400

    braindead question

---
+++ arrays/easy/1929-concat-of-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+        res = []
+        for num in nums:
+            res.append(num)
+        for num in nums:
+            res.append(num)
+        return res
+        
